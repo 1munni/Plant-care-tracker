4cc6c2aaedec3c464ac906fa7cd67be640a1648a
Munni <www.munni76@gmail.com>
2025-05-22 18:25:07 +0600

    lottie added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,44 +3,61 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
+import Lottie from 'lottie-react';
+import plantLottie from '../assets/plants-animation.json'
 
 const Banner = () => {
+  return (
+    <header className="w-full bg-gray-100 border-[2px] border-transparent shadow-xl relative">
+      <Swiper
+        modules={[Navigation]}
+        navigation
+        spaceBetween={30}
+        slidesPerView={1}
+        loop
+        className="relative"
+      >
+        <SwiperSlide>
+          <div className="h-96 flex items-center justify-center">
+            <img
+              src="https://i.ibb.co/fGrYC9sm/indoor-plants-studio.jpg"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-96 flex items-center justify-center">
+            <img
+              src="https://i.ibb.co/35LPKJhN/green-leaves-plant-silver-can-wooden-desk-against-black-background.jpg"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-96 flex items-center justify-center">
+            <img
+              src="https://i.ibb.co/mFbRY49S/front-view-potted-plant-wooden-surface.jpg"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
 
-        return (
-            <header className="w-full bg-gray-100 border-[2px] border-transparent shadow-xl ">
-              <Swiper
-                modules={[Navigation]}
-                navigation
-                spaceBetween={30}
-                slidesPerView={1}
-                loop
-                className='absolute'
-              >
-                <SwiperSlide>
-                  <div className="h-96 flex items-center justify-center text-white text-2xl">
-                    <img src="https://i.ibb.co/fGrYC9sm/indoor-plants-studio.jpg" alt="" />
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="h-96 flex items-center justify-center text-white text-2xl">
-                   <img src="https://i.ibb.co/35LPKJhN/green-leaves-plant-silver-can-wooden-desk-against-black-background.jpg" alt="" />
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="h-96 flex items-center justify-center text-white text-2xl">
-              <img src="https://i.ibb.co/mFbRY49S/front-view-potted-plant-wooden-surface.jpg" alt="" />
-                  </div>
-                </SwiperSlide>
-              </Swiper>
-             <div className='relative -top-57 ml-160 z-1 bg-[#75b72ebc] rounded-2xl hidden lg:inline-block p-5 border-[2px] border-transparent shadow-xl'>
-                <h2 className='text-2xl font-bold text-black '>Let's Explore!
-                    <br />
-                    All of our exclusive plants!
-                </h2>
-             </div>
-            </header>
-          );
-        };
-        
+      {/* Lottie + Text Box */}
+      <div className="absolute top-30 left-80 z-10 bg-[#75b72ebc] rounded-2xl hidden lg:flex items-center gap-4 p-5 border-[2px] border-transparent shadow-xl">
+        <Lottie animationData={plantLottie} className="w-28 h-28" />
+        <div>
+          <h2 className="text-2xl font-bold text-gray leading-snug">
+            Let's Explore!<br />
+            All of our exclusive plants!
+          </h2>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Banner;
