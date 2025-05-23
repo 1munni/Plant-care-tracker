7aafd52ac1825c3b9b8f9c2beba5dfe3149b30d5
Munni <www.munni76@gmail.com>
2025-05-23 18:03:49 +0600

    navbar Drop down fixed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -54,41 +54,79 @@ const NavBar = () => {
 
   return (
   
-         <div  className='bg-base-300 '>
+//          <div  className='bg-base-300 '>
        
-      <div className="navbar mx-auto p-2 max-w-6xl drop-shadow-2xl  ">
-      <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+//       <div className="navbar mx-auto p-2 max-w-6xl drop-shadow-2xl  ">
+//       <div className="navbar-start">
+//           <div className="dropdown">
+//             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+//               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+//             </div>
+//             <ul
+//               tabIndex={0}
+//               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
              
-           {
-            links
-           }
-            </ul>
-          </div>
-          <p><span className='text-[#74b72e] font-bold text-2xl'>Planti</span></p>
+//            {
+//             links
+//            }
+//             </ul>
+//           </div>
+//           <p><span className='text-[#74b72e] font-bold text-2xl'>Planti</span></p>
   
 
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-      {
-        links
-      }
-          </ul>
-        </div>
-        <div className="navbar-end space-x-2 ">
-        <div className='font-bold shadow hidden md:flex'>{user && user.email}</div>
+//         </div>
+//         <div className="navbar-center hidden lg:flex">
+//           <ul className="menu menu-horizontal px-1">
+//       {
+//         links
+//       }
+//           </ul>
+//         </div>
+//         <div className="navbar-end space-x-2 ">
+//         <div className='font-bold shadow hidden md:flex'>{user && user.email}</div>
+// <img
+//   className='w-12 h-12 rounded-full'
+//   src={user ? user.photo : <FaUserCircle />}
+//   alt="User avatar"
+//   title={user ? user.name || "No Name" : "Guest"} 
+// />
+
+
+            
+//             {user ?
+//             (<button onClick={handleLogOut} className='className="btn rounded-2xl bg-[#74b72e] p-2 font-medium"'>LogOut</button>):
+// (<Link to='/login' className="btn rounded-2xl bg-[#74b72e] py-2 font-medium">LogIn</Link>)          }
+          
+//         </div>
+//       </div>
+//       </div>
+<div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        {links}
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">Planti</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+     {links}
+    </ul>
+  </div>
+  
+      <div className="navbar-end space-x-2">
+        <div className='font-bold shadow hidden lg:flex'>{user && user.email}</div>
 <img
   className='w-12 h-12 rounded-full'
-  src={user ? user.photo : <FaUserCircle />}
+  src={user ? user.photoURL : FaUserCircle}
   alt="User avatar"
-  title={user ? user.name || "No Name" : "Guest"} 
+  title={user ? user.displayName || "No Name" : "Guest"} 
 />
 
 
@@ -98,8 +136,7 @@ const NavBar = () => {
 (<Link to='/login' className="btn rounded-2xl bg-[#74b72e] py-2 font-medium">LogIn</Link>)          }
           
         </div>
-      </div>
-      </div>
+</div>
   );
 };
 
