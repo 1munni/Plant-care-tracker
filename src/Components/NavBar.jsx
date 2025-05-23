ee329efc84ff80aadc91dc502129eec3bf0bcfe5
Munni <www.munni76@gmail.com>
2025-05-23 16:43:33 +0600

    login setup

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -53,87 +53,7 @@ const NavBar = () => {
   );
 
   return (
-    // <div className='bg-base-300'>
-    //   <div className='navbar mx-auto p-2 max-w-6xl drop-shadow-2xl'>
-    //     {/* Mobile menu */}
-    //     <div className='navbar-start'>
-    //       <div className='dropdown'>
-    //         <button
-    //           tabIndex={0}
-    //           className='btn btn-ghost lg:hidden'
-    //           type='button'
-    //         >
-    //           <svg
-    //             xmlns='http://www.w3.org/2000/svg'
-    //             className='h-5 w-5'
-    //             fill='none'
-    //             viewBox='0 0 24 24'
-    //             stroke='currentColor'
-    //           >
-    //             <path
-    //               strokeLinecap='round'
-    //               strokeLinejoin='round'
-    //               strokeWidth='2'
-    //               d='M4 6h16M4 12h8m-8 6h16'
-    //             />
-    //           </svg>
-    //         </button>
-    //         <ul
-    //           tabIndex={0}
-    //           className='menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52'
-    //         >
-    //           {links}
-    //         </ul>
-    //       </div>
-    //       <Link to='/' className='ml-2 text-[#74b72e] font-bold text-2xl'>
-    //         Planti
-    //       </Link>
-    //     </div>
-
-    //     {/* Desktop menu */}
-    //     <div className='navbar-center hidden lg:flex'>
-    //       <ul className='menu menu-horizontal px-1'>{links}</ul>
-    //     </div>
-
-    //     {/* User info & auth buttons */}
-    //     <div className='navbar-end flex items-center space-x-3'>
-    //       {user ? (
-    //         <>
-    //           {/* User avatar with tooltip showing email on hover */}
-    //           {user.photo && (
-    //             <div
-    //               className='tooltip tooltip-bottom'
-    //               data-tip={user.email}
-    //             >
-    //               <img
-    //                 src={user.photo}
-    //                 alt='User Avatar'
-    //                 className='w-10 h-10 rounded-full object-cover border-2 border-[#74b72e]'
-    //               />
-    //             </div>
-    //           )}
-
-    //           {/* Logout button */}
-    //           <button
-    //             onClick={handleLogOut}
-    //             className='btn bg-[#74b72e] rounded-xl px-4 text-white'
-    //           >
-    //             Logout
-    //           </button>
-    //         </>
-    //       ) : (
-    //         <>
-    //           <Link to='/login' className='btn bg-[#74b72e] text-white'>
-    //             Login
-    //           </Link>
-    //           <Link to='/register' className='btn btn-outline'>
-    //             Register
-    //           </Link>
-    //         </>
-    //       )}
-    //     </div>
-    //   </div>
-    // </div>
+  
          <div  className='bg-base-300 '>
        
       <div className="navbar mx-auto p-2 max-w-6xl drop-shadow-2xl  ">
@@ -145,6 +65,7 @@ const NavBar = () => {
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+             
            {
             links
            }
@@ -161,8 +82,8 @@ const NavBar = () => {
       }
           </ul>
         </div>
-        <div className="navbar-end space-x-2">
-        <div className='font-bold shadow'>{user && user.email}</div>
+        <div className="navbar-end space-x-2 ">
+        <div className='font-bold shadow hidden md:flex'>{user && user.email}</div>
 <img
   className='w-12 h-12 rounded-full'
   src={user ? user.photo : <FaUserCircle />}
